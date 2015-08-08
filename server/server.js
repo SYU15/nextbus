91e872ac738a49bd2fea8749e3b1f9d649012e14
Sarah Yu <sarah.m.yu88@gmail.com>
2015-08-08 01:12:04 -0700

    (feat) index.html load css files & server.js serves library files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+app.use(express.static(__dirname + '/../client'));
 app.all('/*', function(req, res, next) {
       res.sendFile(path.resolve('client/index.html'));
   });
