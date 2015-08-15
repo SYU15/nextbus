29a8ca70fe9a3fcb90acf46bd37bace5f8aa45bc
Sarah Yu <sarah.m.yu88@gmail.com>
2015-08-15 09:38:44 -0700

    (bugfix) Fix update on all buses with new API call

--- client/js/formInput.js
+++ client/js/formInput.js
@@ -1,19 +1,22 @@
 var updateShowBuses = function() {
-  console.log('called');
-  //hide all buses by default
-  $('rect').hide();
-  //iterate through selected options
-  $('.busOptions :selected').each(function (index, element) {
-    //get value from html option, convert to class
-    var busClass = '.' + $(this).attr('value');
-    //show buses with selected class
-    $(busClass).show();
-  });
+
+  var selectedBuses = $('.busOptions :selected');
+
+  if(selectedBuses.length > 0) {
+    //hide all buses by default
+    $('rect').hide();
+    //iterate through selected options
+    selectedBuses.each(function (index, element) {
+      //get value from html option, convert to class
+      var busClass = '.' + $(this).attr('value');
+      //show buses with selected class
+      $(busClass).show();
+    });    
+  }
 };
 
 $(document).ready(function(){
 
- 
  //listens to see if any options in html form have been clicked
   $('.selectBus').click(function(){
     updateShowBuses();
